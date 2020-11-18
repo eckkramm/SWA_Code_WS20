cd6340410135f5045f698adc64b2c7c26555b479
eckkrammer <eckkramm@technikum-wien.at>
2020-11-18 10:40:43 +0100

    first commit

---
+++ class_24_09/OurFirstApplication/OurFirstApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurFirstApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = "hello World";
+            Console.WriteLine(output);
+            Console.ReadLine();
+           // SOme change
+        }
+    }
+}
