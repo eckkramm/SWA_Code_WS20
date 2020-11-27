e6d276ff411813aaf593534382217c4690fa1e0b
eckkrammer <eckkramm@technikum-wien.at>
2020-11-27 14:30:49 +0100

    end of class 27-11

--- class_27_11/FirstDatatemplate/FirstDatatemplate/ViewModel/ItemVm.cs
+++ class_27_11/FirstDatatemplate/FirstDatatemplate/ViewModel/ItemVm.cs
@@ -4,16 +4,37 @@ namespace FirstDatatemplate.ViewModel
 {
     public class ItemVm : ViewModelBase
     {
+        private decimal price;
+        private int amount;
+
         public string OrderId { get; set; }
         public string Description { get; set; }
-        public int Amount { get; set; }
+        public int Amount
+        {
+            get => amount; set
+            {
+                amount = value;
+                RaisePropertyChanged("FullPrice");
+            }
+        }
         public string Prio { get; set; }
-        public decimal Price { get; set; }
+        public decimal Price
+        {
+            get => price; set
+            {
+                price = value;
+                RaisePropertyChanged("FullPrice");
+            }
+        }
 
         public decimal FullPrice
         {
-            get { return Price*Amount; }
-           
+            get { return Price * Amount; }
+
+        }
+        public ItemVm()
+        {
+            OrderId = "";
         }
 
     }
--- class_27_11/FirstDatatemplate/FirstDatatemplate/ViewModel/MainViewModel.cs
+++ class_27_11/FirstDatatemplate/FirstDatatemplate/ViewModel/MainViewModel.cs
@@ -1,22 +1,72 @@
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
+using System;
 using System.Collections.ObjectModel;
 
 namespace FirstDatatemplate.ViewModel
 {
-  
+
     public class MainViewModel : ViewModelBase
     {
+        private ItemVm newItem;
+
         public ObservableCollection<ItemVm> Items { get; set; }
-        public ItemVm NewItem { get; set; }
+        public ItemVm NewItem { get => newItem; set { newItem = value; RaisePropertyChanged(); } }
         public RelayCommand AddBtnClickCmd { get; set; }
         public RelayCommand DeleteBtnClickCmd { get; set; }
+        public RelayCommand<ItemVm> DeleteBtnClickCmd2 { get; set; }
         public ObservableCollection<string> PrioList { get; set; }
         public ItemVm SelectedItem { get; set; }
 
         public MainViewModel()
         {
+            Items = new ObservableCollection<ItemVm>();
+            if (IsInDesignMode)
+            {
+                GenerateDemoData();
+            }
+           
+            NewItem = new ItemVm();
+
             PrioList = new ObservableCollection<string>() { "Hoch", "Mittel", "Niedrig" };
+            AddBtnClickCmd = new RelayCommand(
+                //Action
+                () =>
+                {
+                    Items.Add(NewItem);
+                    NewItem = new ItemVm();
+                },
+                //CanExecute
+                () => { return NewItem.OrderId.Length > 0; }
+                );
+            DeleteBtnClickCmd = new  RelayCommand(DeleteEntry, CanDeleteEntry);
+            DeleteBtnClickCmd2 = new RelayCommand<ItemVm>(DeleteSpecificEntry);
+        }
+
+        private void DeleteSpecificEntry(ItemVm obj)
+        {
+            Items.Remove(obj);
+        }
+        private void DeleteEntry()
+        {
+            Items.Remove(SelectedItem);
+        }
+
+        private bool CanDeleteEntry()
+        {
+            return SelectedItem != null;
+        }
+
+        private void GenerateDemoData()
+        {
+            Items.Add(new ItemVm() { OrderId = "Demo-01", Description = "Test", Amount = 3, Price = 5, Prio = "Mittel" });
+            Items.Add(new ItemVm() { OrderId = "Demo-02", Description = "Test", Amount = 3, Price = 5, Prio = "Mittel" });
+            Items.Add(new ItemVm() { OrderId = "Demo-03", Description = "Test", Amount = 3, Price = 5, Prio = "Mittel" });
+            Items.Add(new ItemVm() { OrderId = "Demo-04", Description = "Test", Amount = 3, Price = 5, Prio = "Mittel" });
+            Items.Add(new ItemVm() { OrderId = "Demo-05", Description = "Test", Amount = 3, Price = 5, Prio = "Mittel" });
+            Items.Add(new ItemVm() { OrderId = "Demo-06", Description = "Test", Amount = 3, Price = 5, Prio = "Mittel" });
+            Items.Add(new ItemVm() { OrderId = "Demo-07", Description = "Test", Amount = 3, Price = 5, Prio = "Mittel" });
+            Items.Add(new ItemVm() { OrderId = "Demo-08", Description = "Test", Amount = 3, Price = 5, Prio = "Mittel" });
         }
     }
 }
